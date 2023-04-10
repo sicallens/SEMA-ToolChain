1bf810784754878dfaed1f6504af32965fa92807
sicallens <38433389+sicallens@users.noreply.github.com>
2023-04-10 15:53:49 +0200

    add windows simproc (p4)

--- src/ToolChainSCDG/procedures/windows/custom_package/WriteFile.py
+++ src/ToolChainSCDG/procedures/windows/custom_package/WriteFile.py
@@ -1,6 +1,7 @@
 import logging
 import angr
 
+ 
 lw = logging.getLogger("CustomSimProcedureWindows")
 
 
@@ -13,7 +14,6 @@ class WriteFile(angr.SimProcedure):
         lpNumberOfBytesWritten,
         lpOverlapped,
     ):
-
         self.state.project
         simfd = self.state.posix.get_fd(hFile)
         if simfd is None:
@@ -22,10 +22,18 @@ class WriteFile(angr.SimProcedure):
                 "retval_{}".format(self.display_name), self.arch.bits
             )
         bytes_written = simfd.write(lpBuffer, nNumberOfBytesToWrite)
-
         self.state.memory.store(
             lpNumberOfBytesWritten, bytes_written, endness=self.arch.memory_endness
         )
-        # self.state.memory.store(lpNumberOfBytesWritten,self.state.solver.BVS("retval_{}".format(self.display_name),self.arch.bits),endness='Iend_LE')
-        # return self.state.solver.BVS("retval_{}".format(self.display_name),self.arch.bits)
+        lw.info(self.state.globals["files"])
+        lw.info(simfd)
+        lw.info(self.state.solver.eval(hFile))
+        if self.state.solver.eval(hFile) in self.state.globals["files"]:
+            realfd = self.state.globals["files"][self.state.solver.eval(hFile)]
+            lw.info(realfd)
+            if realfd is not None:
+                with open(realfd, "ab") as fd:# TODO fix
+                    content = self.state.solver.eval(self.state.memory.load(lpBuffer,nNumberOfBytesToWrite),cast_to=bytes)
+                    lw.info(content)
+                    fd.write(content)
         return 1
